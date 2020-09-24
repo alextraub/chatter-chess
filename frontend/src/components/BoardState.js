633aa3d8432abcb8dcc4af339bb2deccaa7ea552
Devon <trolle3.14@gmail.com>
2020-09-24 16:37:44 -0500

    The progress so far on the board state
    Sorry for not committing this earlier, hope it helps a little, I'll be finishing it soon.

--- frontend/src/components/BoardState.js
+++ frontend/src/components/BoardState.js
@@ -7,12 +7,58 @@ import Queen from './'
 import Rook from './'
 
 /* This file stores the positions of all current peices on the board 
-and can return a what piece is on a position on the board, all current 
+and can return what piece is on a position on the board, all current 
 board square values, and can change vaules if peices move of change type.*/
 
-class BoardState
+
+constructor(Board)
+{
+    Board = makeChessBoard();
+}
+
+export default function peiceOnPlace()
 {
     
+}
+
+export default function movePeice(from, to)
+{
+    from = PosToNum(from);
+    to = PosToNum(to);
+
+    Board[to] = Board[from];
+    Board[from] = null;
+
+    return 0;
+}
+
+function PosToNum(position)
+{
+    pos1, pos2 = position.split("");
+    fromPos = parseInt(pos2);
+    if(pos1 == "B")
+    {
+        fromPos = fromPos + 8;
+    } else if(pos1 == "C")
+    {
+        fromPos = fromPos + 16;
+    } else if(pos1 == "D")
+    {
+        fromPos = fromPos + 24;
+    } else if(pos1 == "E")
+    {
+        fromPos = fromPos + 32;
+    } else if(pos1 == "F")
+    {
+        fromPos = fromPos + 40;
+    } else if(pos1 == "G")
+    {
+        fromPos = fromPos + 48;
+    } else if(pos1 == "H")
+    {
+        fromPos = fromPos + 56;
+    }
+    return fromPos-1;
 }
 
 export default function makeChessBoard()
