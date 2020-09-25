5b21a995a89c9a5502339ead1b7f2bd25739a441
Raunak Goon <rgoon@uwm.edu>
2020-09-25 09:24:10 -0500

    Update App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import BoardComponent from './BoardComponent'
+
+function click1() {
+    console.log("resetting game");
+    <BoardComponent />
+}
 
 const App = () => (
     <div>
         <h1>Chatter Chess</h1>
-		<small>Play chess with your voice</small>
+        <small>Play chess with your voice</small>
+        < BoardComponent />
+
+        <button onClick={click1}>
+            reset
+        </button>
     </div>
 );
 
