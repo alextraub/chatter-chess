67c56cc029d56a06d84d710167417bda95b0b978
alecschley <aschley@uwm.edu>
2020-09-26 17:03:59 -0500

    worked on Queen class and tests for Queen class

--- frontend/src/game/Piece/Queen/Queen.test.js
+++ frontend/src/game/Piece/Queen/Queen.test.js
@@ -1,1 +1,96 @@
 import Queen from './';
+
+test('Queen can move up more than one space', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 7, col: 3})).toBe(true);
+});
+
+test('Queen can move down more than one space', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 0, col: 3})).toBe(true);
+});
+
+test('Queen can move up one space', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 5, col: 3})).toBe(true);
+});
+
+test('Queen can move down one space', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 3, col: 3})).toBe(true);
+});
+
+test('Queen can move right more than one space', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 4, col: 6})).toBe(true);
+});
+
+test('Queen can move left more than one space', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 4, col: 0})).toBe(true);
+});
+
+test('Queen can move right one space', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 4, col: 4})).toBe(true);
+});
+
+test('Queen can move left one space', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 4, col: 2})).toBe(true);
+});
+
+test('Queen can move diagonal up right more than one space', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 7, col: 6})).toBe(true);
+});
+
+test('Queen can move diagonal up left more than one space', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 7, col: 0})).toBe(true);
+});
+
+test('Queen can move diagonal up right one space', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 5, col: 4})).toBe(true);
+});
+
+test('Queen can move diagonal up left one space', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 5, col: 2})).toBe(true);
+});
+
+test('Queen can move diagonal down right more than one space', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 1, col: 6})).toBe(true);
+});
+
+test('Queen can move diagonal down left more than one space', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 1, col: 0})).toBe(true);
+});
+
+test('Queen can move diagonal down right one space', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 3, col: 4})).toBe(true);
+});
+
+test('Queen can move diagonal down left one space', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 3, col: 2})).toBe(true);
+});
+
+test('Queen illegal move', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 5, col: 7})).toBe("The Queen cannot move into that position");
+});
+
+test('Queen illegal move #2', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 5, col: 0})).toBe("The Queen cannot move into that position");
+});
+
+test('Queen illegal move #3', () => {
+	const queen = new Queen({row: 4, col: 3});
+	expect(queen.canMoveTo({row: 2, col: 4})).toBe("The Queen cannot move into that position");
+});
--- frontend/src/game/Piece/Queen/index.js
+++ frontend/src/game/Piece/Queen/index.js
@@ -1,5 +1,33 @@
 import Piece from '../Piece';
 
 export default class Queen extends Piece {
+	constructor(position, player) {
+		super(position, player);
+	}
 
+	canMoveTo({row, col}) {
+		if (row !== this.position.row) {
+			// up/down movement
+			if (col === this.position.col) {
+				return true;
+			} else {
+				const rowDif = this.position.row - row;
+				const colDif = this.position.col - col;
+				if (Math.abs(rowDif) !== Math.abs(colDif)) {
+					return "The Queen cannot move into that position";
+				} else {
+					return true;
+				}
+			}
+		} else if (row === this.position.row) {
+			// left/right movement
+			if (col !== this.position.col) {
+				return true;
+			} else {
+				return "The King cannot move into that position";
+			}
+		} else {
+			return "The King cannot move into that position";
+		}
+	}
 }
