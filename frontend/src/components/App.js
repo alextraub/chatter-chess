e944b0586cd235a57ee2be20bebb21c58a9faa80
Raunak Goon <rgoon@uwm.edu>
2020-09-29 00:05:58 -0500

    Update App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,41 @@
 import React from "react";
+
 import MoveInput from "./MoveInput";
 
 const App = () => (
     <div>
         <h1>Chatter Chess</h1>
-		<small>Play chess with your voice</small>
-		<MoveInput />
+        <small>Play chess with your voice</small>
+        <MoveInput />
+    </div>
+);
+
+export default App;
+
+import React from "react";
+import BoardComponent from './BoardComponent'
+import MoveInput from "./MoveInput";
+
+function click() {
+    console.log('you lose');
+}
+function click1() {
+    console.log("resetting game");
+    <BoardComponent />
+}
+
+const App = () => (
+    <div>
+        <h1>Chatter Chess</h1>
+        <small>Play chess with your voice</small>
+        < BoardComponent />
+        <button onClick={click}>
+            forfeit
+        </button>
+        <button onClick={click1}>
+            reset
+        </button>
+        <MoveInput />
     </div>
 );
 
