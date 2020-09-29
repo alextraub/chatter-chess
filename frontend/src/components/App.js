16cfb1ee62e53401d47dbf6eb42e73ff02e5a8d3
Raunak Goon <rgoon@uwm.edu>
2020-09-29 00:10:17 -0500

    Update App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,4 @@
 import React from "react";
-
-import MoveInput from "./MoveInput";
-
-const App = () => (
-    <div>
-        <h1>Chatter Chess</h1>
-        <small>Play chess with your voice</small>
-        <MoveInput />
-    </div>
-);
-
-export default App;
-
-import React from "react";
 import BoardComponent from './BoardComponent'
 import MoveInput from "./MoveInput";
 
