ed14eb7f905af1903fa9e020ddd996d153431753
alecschley <aschley@uwm.edu>
2020-09-29 22:04:06 -0500

    worked on Queen class

--- frontend/src/game/Piece/Queen/index.js
+++ frontend/src/game/Piece/Queen/index.js
@@ -6,9 +6,105 @@ export default class Queen extends Piece {
 		if(!validFinalPosition) {
 			return false;
 		}
-		const numRows = Math.abs(toRow - fromRow);
-		const numCols = Math.abs(toCol - fromCol);
+		const numRows = toRow - fromRow;
+		const numCols = toCol - fromCol;
+		// diagonal up right
+		if (numRows > 0 && numCols > 0 && Math.abs(numRows) === Math.abs(numCols)) {
+			let currR = fromRow + 1;
+			let currC = fromCol + 1;
+			while (currR < toRow && currC < toCol) {
+				if (super.boardState.getPiece(currR, currC) !== null) {
+					return false;
+				}
+				currR++;
+				currC++;
+			}
+			return true;
 
-		return ((numRows === numCols) && (numRows !== 0) && (numCols !== 0)) || (numRows > 0 && numCols === 0) || (numRows === 0 && numCols > 0);
+		// diagonal up left
+		} else if (numRows > 0 && numCols < 0 && Math.abs(numRows) === Math.abs(numCols)) {
+			let currR = fromRow + 1;
+			let currC = fromCol - 1;
+			while (currR < toRow && currC > toCol) {
+				if (super.boardState.getPiece(currR, currC) !== null) {
+					return false;
+				}
+				currR++;
+				currC--;
+			}
+			return true;
+
+		// diagonal down right
+		} else if (numRows < 0 && numCols > 0 && Math.abs(numRows) === Math.abs(numCols)) {
+			let currR = fromRow - 1;
+			let currC = fromCol + 1;
+			while (currR > toRow && currC < toCol) {
+				if (super.boardState.getPiece(currR, currC) !== null) {
+					return false;
+				}
+				currR--;
+				currC++;
+			}
+			return true;
+
+		// diagonal down left
+		} else if (numRows < 0 && numCols < 0 && Math.abs(numRows) === Math.abs(numCols)) {
+			let currR = fromRow - 1;
+			let currC = fromCol - 1;
+			while (currR > toRow && currC > toCol) {
+				if (super.boardState.getPiece(currR, currC) !== null) {
+					return false;
+				}
+				currR--;
+				currC--;
+			}
+			return true;
+
+		// up
+		} else if (numRows > 0 && numCols === 0) {
+			let currR = fromRow + 1;
+			while (currR < toRow) {
+				if (super.boardState.getPiece(currR, fromCol) !== null) {
+					return false;
+				}
+				currR++;
+			}
+			return true;
+
+		// down
+		} else if (numRows < 0 && numCols === 0) {
+			let currR = fromRow - 1;
+			while (currR > toRow) {
+				if (super.boardState.getPiece(currR, fromCol) !== null) {
+					return false;
+				}
+				currR--;
+			}
+			return true;
+
+		// right
+		}  else if (numRows === 0 && numCols > 0) {
+			let currC = fromCol + 1;
+			while (currC < toCol) {
+				if (super.boardState.getPiece(fromRow, currC) !== null) {
+					return false;
+				}
+				currC++;
+			}
+			return true;
+
+		// left
+		} else if (numRows === 0 && numCols < 0) {
+			let currC = fromCol + 1;
+			while (currC > toCol) {
+				if (super.boardState.getPiece(fromRow, currC) !== null) {
+					return false;
+				}
+				currC--;
+			}
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
