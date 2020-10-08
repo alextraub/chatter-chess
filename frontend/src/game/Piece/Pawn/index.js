ae2f2ce1afba62f5c3a1ebcb757efc18415d7576
Raunak Goon <rgoon@uwm.edu>
2020-10-07 22:31:31 -0500

    Update index.js

--- frontend/src/game/Piece/Pawn/index.js
+++ frontend/src/game/Piece/Pawn/index.js
@@ -1,5 +1,112 @@
-import Piece from '../Piece';
+import Piece from './';
 
 export default class Pawn extends Piece {
+    canMove([fromRow, fromCol], [toRow, toCol]) {
+        if (!validFinalPosition) {
+            return false;
+        }
+        const numRows = toRow - fromRow;
+        const numCols = toCol - fromCol;
 
+
+
+        //    ****** for the pawn moving up 1 *******     \\
+        if (this.Piece.isWhite() || this.Piece.isBlack()) {
+            if (numRows === 1) {
+                if (numCols === 0) {
+                    let currentR = fromRow + 1;
+                    //if that position is not empty and if the piece is white piece or black piece
+                    if (this.board.getPiece(currentR, fromCol) != null && (this.isWhite() || this.isBlack())) {
+                        return false;
+                    }
+                    return (numRows === 1 && numCols === 0)
+                }
+            }
+        }
+        //******************************************** */
+
+        /*
+       
+            *******************************************************************
+                   *********** for the pawn moving up by 2 ********
+                   
+        */
+        if ((fromRow == 0 && toRow == 0) && (fromCol === 0 && toCol === 0))
+            if (numRows === 2) {
+                if (numCols === 0) {
+                    let currentR = fromRow + 2;
+                    if (this.board.getPiece(currentR, fromCol) != null) { //if that position is not empty
+                        return false;
+                    }
+                    return (numRows === 2 && numCols === 0)
+                }
+            }
+
+
+        //   ******  pawn diagonal up left ************* \\
+        if (this.isBlack()) {
+            if (numRows === 1) {
+                if (numCols === -1) {
+                    let currentR = fromRow + 1;
+                    let currentC = fromCol - 1;
+                    if (this.board.getPiece(currentR, currentC) != null) {
+                        if (this.isWhite()) {
+                            return (numRows === 1 && numCols === -1)
+                        }
+                        return false;
+                    }
+                }
+            }
+        }
+
+        else if (this.isWhite()) {
+            if (numRows === 1) {
+                if (numCols === -1) {
+                    let currR = fromRow + 1;
+                    let currC = fromCol - 1;
+                    if (this.board.getPiece(currR, currC) != null) {
+                        if (this.isWhite()) {
+                            return false;
+                        }
+                        return (numRows === 1 && numCols === -1);
+                    }
+                }
+            }
+        }
+
+
+        //  *******************************************************************
+
+        //    Diagonal right:
+
+        if (this.isBlack()) {
+            if (numRows === 1) {
+                if (numCols === 1) {
+                    let currentR = fromRow + 1;
+                    let currentC = fromCol + 1;
+                    if (this.board.getPiece(currentR, currentC) != null) {
+                        if (this.isWhite()) {
+                            return (numRows === 1 && numCols === 1)
+                        }
+                        return false;
+                    }
+                }
+            }
+        }
+
+        else if (this.isWhite()) {
+            if (numRows === 1) {
+                if (numCols === 1) {
+                    let currR = fromRow + 1;
+                    let currC = fromCol + 1;
+                    if (this.board.getPiece(currR, currC) != null) {
+                        if (this.isWhite()) {
+                            return false;
+                        }
+                        return (numRows === 1 && numCols === 1);
+                    }
+                }
+            }
+        }
+    }
 }
