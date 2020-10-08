0f3ddc2530e2d2e4da8dd53e2501abb1bc05eb0f
Raunak Goon <rgoon@uwm.edu>
2020-10-07 20:29:11 -0500

    Updated changes to board comp

--- frontend/src/components/BoardComponent/index.js
+++ frontend/src/components/BoardComponent/index.js
@@ -5,39 +5,34 @@ import BoardSquare from '../BoardSquare';
 import Piece from '../../game/Piece/Piece';
 
 export default class BoardComponent extends React.Component {
-	static propTypes = {
-		playerView: PropTypes.oneOf([0, 1]),
-		board: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.instanceOf(Piece)))
-	}
+    static propTypes = {
+        playerView: PropTypes.oneOf([0, 1]),
+        board: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.instanceOf(Piece)))
+    }
 
     render() {
         let squares = [];
-        let count = 1;
         for (let row = 0; row < 8; row++) {
             for (let col = 0; col < 8; col++) {
-				const piece = this.props.board[row][col];
+                const piece = this.props.board[row][col];
                 if (row % 2 === 0) {
                     if (col % 2 === 0) {
-                        squares.push(<BoardSquare black={false} piece={piece} />)
+                        squares.push(<BoardSquare key={`${row}-${col}`} piece={piece} />)
                     }
                     else {
-                        squares.push(<BoardSquare black={true} piece={piece} />)
+                        squares.push(<BoardSquare key={`${row}-${col}`} black piece={piece} />)
                     }
                 }
                 if (row % 2 === 1) {
                     if (col % 2 === 0) {
-                        squares.push(<BoardSquare black={true} piece={piece} />)
+                        squares.push(<BoardSquare key={`${row}-${col}`} black piece={piece} />)
 
                     }
-                    else
-                        squares.push(<BoardSquare black={false} piece={piece} />)
-
                 }
             }
         }
-
         return (
-            <div className="board" > {squares}</div>
+            <div className="board" > {this.props.player === 0 ? squares : squares.reverse}</div>
         );
 
     }
