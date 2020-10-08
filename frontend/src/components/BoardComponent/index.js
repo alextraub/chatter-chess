5811494542055128783d82e229de2784aaf38527
Alex Traub <abtraub@uwm.edu>
2020-10-07 21:12:15 -0500

    More merge conflict resolutions

--- frontend/src/components/BoardComponent/index.js
+++ frontend/src/components/BoardComponent/index.js
@@ -5,17 +5,10 @@ import BoardSquare from '../BoardSquare';
 import Piece from '../../game/Piece/Piece';
 
 export default class BoardComponent extends React.Component {
-<<<<<<< HEAD
-    static propTypes = {
-        playerView: PropTypes.oneOf([0, 1]),
-        board: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.instanceOf(Piece)))
-    }
-=======
 	static propTypes = {
 		playerView: PropTypes.oneOf([0, 1]),
 		board: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.instanceOf(Piece)))
 	}
->>>>>>> c64430f9ad6b0a85ba0c355bbd85f3c5a40750b6
 
     render() {
         let squares = [];
