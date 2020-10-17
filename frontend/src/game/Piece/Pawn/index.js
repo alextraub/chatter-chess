236f31058b38b6245eaf4b024cd66fae98e16253
alecschley <aschley@uwm.edu>
2020-10-17 14:25:47 -0500

    worked on pawn movement

--- frontend/src/game/Piece/Pawn/index.js
+++ frontend/src/game/Piece/Pawn/index.js
@@ -2,105 +2,44 @@ import Piece from './';
 
 export default class Pawn extends Piece {
     canMove([fromRow, fromCol], [toRow, toCol]) {
-        if (!validFinalPosition) {
+		const validFinalPosition = super.canMove([ fromRow, fromCol ], [ toRow, toCol ]);
+    	if (!validFinalPosition) {
             return false;
         }
         const numRows = toRow - fromRow;
         const numCols = toCol - fromCol;
 
-
-
         //    ****** for the pawn moving up 1 *******     \\
-        if (this.Piece.isWhite() || this.Piece.isBlack()) {
-            if (numRows === 1) {
-                if (numCols === 0) {
-                    //if that position is not empty and if the piece is white piece or black piece
-                    if (this.board.getPiece(numRows, numCols) != null && (this.isWhite() || this.isBlack())) {
-                        return false;
-                    }
-                    return (numRows === 1 && numCols === 0);
-                }
-            }
-        }
-        //******************************************** */
 
-        /*
-       
-            *******************************************************************
-                   *********** for the pawn moving up by 2 ********
-                   
-        */
-        if ((fromRow == 0 && toRow == 0) && (fromCol === 0 && toCol === 0))
-            let currentR = fromRow + 2;
-        let currentCol = fromCol;
-        let thisRow = fromRow + 1;
-        let thisCol = fromCol;
-        if (this.board.getPiece(thisRow, thisCol) != null) { //if that 1 up position is not empty
-            return false;
-        }
-        if (this.board.getPiece(currentR, currentCol) != null) { // if that 2nd up position isn't empty 
-            return false;
-        }
-        return (currentR && currentCol); // 
+            if ((Math.abs(numRows) === 1 || Math.abs(numRows) === 2) && numCols === 0) {
+            		if (numRows < 0 && this.isBlack()) {
+            			return false;
+					}
+            		if (numRows > 0 && this.isWhite()) {
+            			return false;
+					}
+            		if (Math.abs(numRows) === 2) {
+						if (fromRow === 1 && this.isWhite()) {
+							if (this.board.getPiece(fromRow - 1, fromCol) !== null) {
+								return false;
+							}
+						}
+						if (fromRow === 6 && this.isBlack()) {
+							if (this.board.getPiece(fromRow + 1, fromCol) !== null) {
+								return false;
+							}
+						}
+					}
+                    //if the to position is empty, return true
+                    return this.board.getPiece(numRows, numCols) == null;
+            }
+            if (Math.abs(numCols) === 1) {
+            	if (numRows === -1 && this.isBlack()) {
+					return false;
+				} else if (numRows === 1 && this.isWhite()) {
+            		return false;
+				}
+				return (this.boardState.getPiece(numRows, numCols) != null);
+			}
     }
 }
-
-
-//   ******  pawn diagonal up left ************* \\
-if (this.isBlack()) {
-    if (numRows === 1) {
-        if (numCols === -1) {
-
-            if (this.board.getPiece(numRows, numCols) != null) {
-                if (this.isWhite()) {
-                    return (numRows === 1 && numCols === -1);
-                }
-                return false;
-            }
-        }
-    }
-}
-
-else if (this.isWhite()) {
-    if (numRows === 1) {
-        if (numCols === -1) {
-            if (this.board.getPiece(numRows, numCols) != null) {
-                if (this.isWhite()) {
-                    return false;
-                }
-                return (numRows === 1 && numCols === -1)
-            }
-        }
-    }
-}
-
-
-//  *******************************************************************
-
-//    Diagonal right:
-
-if (this.isBlack()) {
-    if (numRows === 1) {
-        if (numCols === 1) {
-            if (this.board.getPiece(numRows, numCols) != null) {
-                if (this.isWhite()) {
-                    return (numRows === 1 && numCols === 1);
-                }
-                return false;
-            }
-        }
-    }
-}
-
-else if (this.isWhite()) {
-    if (numRows === 1) {
-        if (numCols === 1) {
-            if (this.board.getPiece(numRows, numCols) != null) {
-                if (this.isWhite()) {
-                    return false;
-                }
-                return (numRows === 1 && numCols === 1);
-            }
-        }
-    }
-} 
