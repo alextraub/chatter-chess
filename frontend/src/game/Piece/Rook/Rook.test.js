8c0bea34f980001813aea44c01028921201055f7
Devon <trolle3.14@gmail.com>
2020-10-21 16:05:27 -0500

    Missed some stuff in rook
    forgot to save before pushing

--- frontend/src/game/Piece/Rook/Rook.test.js
+++ frontend/src/game/Piece/Rook/Rook.test.js
@@ -23,5 +23,64 @@ test('Rook can move right', () =>
 {
     moveToEmptySquare();
 
-    expect(whiteRook.canMove([0, 0], []))
-})
+    expect(whiteRook.canMove([0, 0], [0, 1])).toBe(true);
+    expect(blackRook.canMove([1, 0], [1, 1])).toBe(true);
+});
+
+test('Rook can move down', () =>
+{
+    moveToEmptySquare();
+
+    expect(whiteRook.canMove([0, 0], [1, 0])).toBe(true);
+    expect(blackRook.canMove([0, 1], [1, 1])).toBe(true);
+});
+
+test('Rook and move left', () =>
+{
+    moveToEmptySquare();
+
+    expect(whiteRook.canMove([0, 1], [0, 0])).toBe(true);
+    expect(blackRook.canMove([1, 1], [1, 0])).toBe(true);
+});
+
+test('Rook can move up', () =>
+{
+    moveToEmptySquare();
+
+    expect(whiteRook.canMove([1, 0], [0, 0])).toBe(true);
+    expect(blackRook.canMove([1, 1], [1, 0])).toBe(true);
+});
+
+test('Rook can move to a square with a enemy piece', () =>
+{
+    boardState.getPiece.mockReturnValueOnce(blackPiece).mockReturnValue(whitePiece);
+
+    expect(whiteRook.canMove([0, 0], [0, 1])).toBe(true);
+    expect(blackRook.canMove([0, 0], [0, 1])).toBe(true);
+});
+
+test('Rook can NOT move to a square with an allied piece', () =>
+{
+    boardState.getPiece.mockReturnValueOnce(whitePiece).mockReturnValue(blackPiece);
+
+    expect(whiteRook.canMove([0, 0], [0, 1])).toBe(false);
+    expect(blackRook.canMove([0, 0], [0, 1])).toBe(false);
+});
+
+test('Rook can NOT move to the same space it is on', () =>
+{
+    moveToEmptySquare();
+
+    expect(whiteRook.canMove([1, 1], [1, 1])).toBe(false);
+});
+
+test('Rook can NOT move OVer pieces', () =>
+{
+    boardState.getPiece.mockReturnValue(blackPiece);
+
+    expect(whiteRook.canMove([0, 0], [0, 2])).toBe(false);
+
+    boardState.getPiece.mockReturnValue(whitePiece);
+
+    expect(blackRook.canMove([0, 0], [0, 2])).toBe(false);
+});
