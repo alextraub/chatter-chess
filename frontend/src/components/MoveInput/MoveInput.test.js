6f3e173c0442df5f2b5003b9e6405afd2f3cf4f0
alecschley <aschley@uwm.edu>
2020-10-24 17:06:18 -0500

    changed CapturedPieces, MoveInput and tests

--- frontend/src/components/MoveInput/MoveInput.test.js
+++ frontend/src/components/MoveInput/MoveInput.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { render, cleanup, fireEvent } from '@testing-library/react';
+import { render, cleanup, fireEvent} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import renderer from 'react-test-renderer';
 import MoveInput from "./";
 
 afterEach(cleanup);
@@ -21,97 +20,129 @@ test('User can enter a move', () => {
 });
 
 test('Validates that move is in correct form', () => {
-	let component = renderer.create(<MoveInput/>).getInstance();
-	component.state.move = "A1 B2";
-	component.validate();
-	expect(component.state.move).toBe("A1 B2");
-	expect(component.state.from).toBe("A1");
-	expect(component.state.to).toBe("B2");
-	expect(component.state.moveError).toBe("");
-	component.state.from = "";
-	component.state.to = "";
+	const { getByTestId, getByText } = render(
+		<MoveInput/>
+	);
+	fireEvent.change(getByTestId("move"), {
+		target: {
+			value: "A1 B2"
+		}
+	});
+	expect(getByTestId('move').value).toBe('A1 B2');
+	fireEvent.click(getByTestId('button'));
+	expect(getByText('Invalid Move')).toBeFalsy();
 });
 
 test('Validates you can enter a correct move in lowercase as well', () => {
-	let component = renderer.create(<MoveInput/>).getInstance();
-	component.state.move = "a1 b2";
-	component.validate();
-	expect(component.state.move).toBe("a1 b2");
-	expect(component.state.from).toBe("a1");
-	expect(component.state.to).toBe("b2");
-	expect(component.state.moveError).toBe("");
-	component.state.from = "";
-	component.state.to = "";
+	const { getByTestId, getByText } = render(
+		<MoveInput/>
+	);
+	fireEvent.change(getByTestId("move"), {
+		target: {
+			value: "a1 b2"
+		}
+	});
+	expect(getByTestId('move').value).toBe('a1 b2');
+	fireEvent.click(getByTestId('button'));
+	expect(getByText('Invalid Move')).toBeFalsy();
 });
 
 test('Validates that move is in incorrect form', () => {
-	let component = renderer.create(<MoveInput/>).getInstance();
-	component.state.move = "1A 2B";
-	component.validate();
-	expect(component.state.move).toBe("1A 2B");
-	expect(component.state.from).toBe("");
-	expect(component.state.to).toBe("");
-	expect(component.state.moveError).toBe("Invalid Move");
+	const { getByTestId, getByText } = render(
+		<MoveInput/>
+	);
+	fireEvent.change(getByTestId("move"), {
+		target: {
+			value: "1A 2B"
+		}
+	});
+	expect(getByTestId('move').value).toBe('1A 2B');
+	fireEvent.click(getByTestId('button'));
+	expect(getByText('Invalid Move')).toBeTruthy();
 });
 
 test('Validates that move is out of range', () => {
-	let component = renderer.create(<MoveInput/>).getInstance();
-	component.state.move = "J9 Z0";
-	component.validate();
-	expect(component.state.move).toBe("J9 Z0");
-	expect(component.state.from).toBe("");
-	expect(component.state.to).toBe("");
-	expect(component.state.moveError).toBe("Invalid Move");
+	const { getByTestId, getByText } = render(
+		<MoveInput/>
+	);
+	fireEvent.change(getByTestId("move"), {
+		target: {
+			value: "J9 Z7"
+		}
+	});
+	expect(getByTestId('move').value).toBe('J9 Z7');
+	fireEvent.click(getByTestId('button'));
+	expect(getByText('Invalid Move')).toBeTruthy();
 });
 
-test('Validates that move has 2 positions', () => {
-	let component = renderer.create(<MoveInput/>).getInstance();
-	component.state.move = "A1 B2 C3";
-	component.validate();
-	expect(component.state.move).toBe("A1 B2 C3");
-	expect(component.state.from).toBe("");
-	expect(component.state.to).toBe("");
-	expect(component.state.moveError).toBe("Invalid Move");
+test('Validates that move cannot have 3 positions', () => {
+	const { getByTestId, getByText } = render(
+		<MoveInput/>
+	);
+	fireEvent.change(getByTestId("move"), {
+		target: {
+			value: "A1 B2 C3"
+		}
+	});
+	expect(getByTestId('move').value).toBe('A1 B2 C3');
+	fireEvent.click(getByTestId('button'));
+	expect(getByText('Invalid Move')).toBeTruthy();
 });
 
-test('Validates that move doesnt have 1 position', () => {
-	let component = renderer.create(<MoveInput/>).getInstance();
-	component.state.move = "A1";
-	component.validate();
-	expect(component.state.move).toBe("A1");
-	expect(component.state.from).toBe("");
-	expect(component.state.to).toBe("");
-	expect(component.state.moveError).toBe("Invalid Move");
+test('Validates that move cannot have 1 position', () => {
+	const { getByTestId, getByText } = render(
+		<MoveInput/>
+	);
+	fireEvent.change(getByTestId("move"), {
+		target: {
+			value: "A1"
+		}
+	});
+	expect(getByTestId('move').value).toBe('A1');
+	fireEvent.click(getByTestId('button'));
+	expect(getByText('Invalid Move')).toBeTruthy();
 });
 
 test('Validates that moves dont have more than 2 characters', () => {
-	let component = renderer.create(<MoveInput/>).getInstance();
-	component.state.move = "A12 B23";
-	component.validate();
-	expect(component.state.move).toBe("A12 B23");
-	expect(component.state.from).toBe("");
-	expect(component.state.to).toBe("");
-	expect(component.state.moveError).toBe("Invalid Move");
+	const { getByTestId, getByText } = render(
+		<MoveInput/>
+	);
+	fireEvent.change(getByTestId("move"), {
+		target: {
+			value: "A12 B23"
+		}
+	});
+	expect(getByTestId('move').value).toBe('A12 B23');
+	fireEvent.click(getByTestId('button'));
+	expect(getByText('Invalid Move')).toBeTruthy();
 });
 
 test('Validates that moves dont have less than 2 characters', () => {
-	let component = renderer.create(<MoveInput/>).getInstance();
-	component.state.move = "A B";
-	component.validate();
-	expect(component.state.move).toBe("A B");
-	expect(component.state.from).toBe("");
-	expect(component.state.to).toBe("");
-	expect(component.state.moveError).toBe("Invalid Move");
+	const { getByTestId, getByText } = render(
+		<MoveInput/>
+	);
+	fireEvent.change(getByTestId("move"), {
+		target: {
+			value: "A B"
+		}
+	});
+	expect(getByTestId('move').value).toBe('A B');
+	fireEvent.click(getByTestId('button'));
+	expect(getByText('Invalid Move')).toBeTruthy();
 });
 
 test('Validates that move has been entered', () => {
-	let component = renderer.create(<MoveInput/>).getInstance();
-	component.state.move = "";
-	component.validate();
-	expect(component.state.move).toBe("");
-	expect(component.state.from).toBe("");
-	expect(component.state.to).toBe("");
-	expect(component.state.moveError).toBe("You need to enter a move");
+	const { getByTestId, getByText } = render(
+		<MoveInput/>
+	);
+	fireEvent.change(getByTestId("move"), {
+		target: {
+			value: ""
+		}
+	});
+	expect(getByTestId('move').value).toBe('');
+	fireEvent.click(getByTestId('button'));
+	expect(getByText('Invalid Move')).toBeTruthy();
 });
 
 
