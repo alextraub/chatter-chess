5e575fabd2c956aa88b81bad26242370905fea76
Raunak Goon <rgoon@uwm.edu>
2020-10-27 19:00:25 -0500

    Update Pawn.test.js

--- frontend/src/game/Piece/Pawn/Pawn.test.js
+++ frontend/src/game/Piece/Pawn/Pawn.test.js
@@ -1,10 +1,10 @@
 import Pawn from './';
-import BoardState, { mockGetPiece } from '../../../__mocks__/boardStateMock';
 import DPiece from '../../../__mocks__/DPiece';
+import BoardState from '../../BoardState';
+jest.mock('../../BoardState');
 
 beforeEach(() => {
     BoardState.mockClear();
-    mockGetPiece.mockClear();
 });
 
 const boardState = new BoardState();
@@ -14,7 +14,7 @@ const whitePiece = new DPiece(boardState);
 const blackPiece = new DPiece(boardState, 1);
 
 const moveToEmptySquare = () => {
-    mockGetPiece.mockReturnValue(null);
+    boardState.getPiece.mockReturnValue(null);
 }
 
 test('pawn can move up 1 square', () => {
@@ -29,22 +29,26 @@ test('pawn can move up 2 square', () => {
     expect(blackPawn.canMove([3, 4], [5, 4])).toBe(true);
 });
 
-test('pawn can move to a square with an enemy piece right diagonal', () => {
-    mockGetPiece.mockReturnValueOnce(blackPiece).mockReturnValue(whitePiece);
-
+test('pawn can move to a square with an enemy piece diagonal', () => {
+    boardState.getPiece.mockReturnValueOnce(blackPiece).mockReturnValue(whitePiece);
     expect(whitePawn.canMove([0, 0], [1, 1])).toBe(true);
     expect(blackPawn.canMove([0, 0], [1, 1])).toBe(true);
 });
 
 test('Pawn cannot move to a square with a piece belonging to the same player', () => {
-    mockGetPiece.mockReturnValueOnce(whitePiece).mockReturnValue(blackPiece);
+    boardState.getPiece.mockReturnValueOnce(whitePiece).mockReturnValue(blackPiece);
 
-    expect(whiteKnight.canMove([2, 1], [3, 1])).toBe(false);
-    expect(blackKnight.canMove([2, 1], [3, 1])).toBe(false);
+    expect(whitePawn.canMove([2, 1], [3, 1])).toBe(false);
+    expect(blackPawn.canMove([2, 1], [3, 1])).toBe(false);
 });
 
 test('pawn cannot move horizontally', () => {
     moveToEmptySquare();
     expect(blackPawn.canMove([0, 0], [0, 2])).toBe(false);
     expect(whitePawn.canMove([0, 5], [0, 1])).toBe(false);
+});
+
+test('Pawn cannot move to the same position', () => {
+    moveToEmptySquare();
+    expect(whitePawn.canMove([0, 0], [0, 0])).toBe(false);
 });
