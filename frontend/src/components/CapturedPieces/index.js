175728abc17c4718c2bb1c0162462a23db24f893
alecschley <aschley@uwm.edu>
2020-10-29 20:07:07 -0500

    worked on CapturedPieces

--- frontend/src/components/CapturedPieces/index.js
+++ frontend/src/components/CapturedPieces/index.js
@@ -1,72 +1,82 @@
 import React from 'react';
 import { pieceName } from '../../game/Piece';
 import {blackPieceImages, whitePieceImages} from "../ChessPiece/pieceImages";
+import ChessPiece from '../ChessPiece';
 
 export default class CapturedPieces extends React.Component {
 	constructor(props) {
 		super(props);
+		// this.state = {
+		// 	pieceCounts: {'rook': 0}
+		// };
 	}
+
+	// renderPieces(black) {
+	// 	return this.state.pieceCounts.entries().map([key, value] => (
+	// 		<div key={key}>
+	// 			<ChessPiece type={key} black={black}/>
+	// 			{value}
+	// 		</div>
+	// 	));
+	// }
 
 	render() {
 		return (
 			<div id='capturedContainer'>
-				<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Creepster+Caps" />
 				<h1>Shadow Realm</h1><br/>
 				<h2>Captured White Pieces: {this.props.whitePieces.length}</h2><br/>
-
-					{this.props.whitePieces.map((piece) => {
+					{Array.from(this.props.whitePieces).map((piece) => {
 						if (pieceName(piece) === "pawn")
 							return (
-								<img src={whitePieceImages.pawn} alt={"pawn"}/>
+								<img src={whitePieceImages.pawn} alt={"wpawn"}/>
 							);
 						else if (pieceName(piece) === "rook")
 							return (
-								<img src={whitePieceImages.rook} alt={"rook"}/>
+								<img src={whitePieceImages.rook} alt={"wrook"}/>
 							);
 						else if (pieceName(piece) === "bishop")
 							return (
-								<img src={whitePieceImages.bishop} alt={"bishop"}/>
+								<img src={whitePieceImages.bishop} alt={"wbishop"}/>
 							);
 						else if (pieceName(piece) === "knight")
 							return (
-								<img src={whitePieceImages.knight} alt={"knight"}/>
+								<img src={whitePieceImages.knight} alt={"wknight"}/>
 							);
 						else if (pieceName(piece) === "queen")
 							return (
-								<img src={whitePieceImages.queen} alt={"queen"}/>
+								<img src={whitePieceImages.queen} alt={"wqueen"}/>
 							);
 						else if (pieceName(piece) === "king")
 							return (
-								<img src={whitePieceImages.king} alt={"king"}/>
+								<img src={whitePieceImages.king} alt={"wking"}/>
 							);
 					})}
 
 				<h2>Captured Black Pieces: {this.props.blackPieces.length}</h2><br/>
-
-					{this.props.blackPieces.map((piece) => {
+					{Array.from(this.props.blackPieces).map((piece) => {
 						if (pieceName(piece) === "pawn")
 							return (
-								<img src={blackPieceImages.pawn} alt={"pawn"}/>
+								<img src={blackPieceImages.pawn} alt={"bpawn"}/>
 							);
 						else if (pieceName(piece) === "rook")
 							return (
-								<img src={blackPieceImages.rook} alt={"rook"}/>
+								<img src={blackPieceImages.rook} alt={"brook"}/>
 							);
 						else if (pieceName(piece) === "bishop")
 							return (
-								<img src={blackPieceImages.bishop} alt={"bishop"}/>
+								<img src={blackPieceImages.bishop} alt={"bbishop"}/>
 							);
 						else if (pieceName(piece) === "knight")
 							return (
-								<img src={blackPieceImages.knight} alt={"knight"}/>
+								<img src={blackPieceImages.knight} alt={"bknight"}/>
 							);
 						else if (pieceName(piece) === "queen")
 							return (
-								<img src={blackPieceImages.queen} alt={"queen"}/>
+								<img src={blackPieceImages.queen} alt={"bqueen"}/>
 							);
 						else if (pieceName(piece) === "king")
 							return (
-								<img src={blackPieceImages.king} alt={"king"}/>
+								<img src={blackPieceImages.king} alt={"bking"}/>
 							);
 					})}
 
