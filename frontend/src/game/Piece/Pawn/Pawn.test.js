dc11e004a2d8a68418e856fd195c03133e25a070
Raunak Goon <rgoon@uwm.edu>
2020-10-29 21:44:42 -0500

    Update Pawn.test.js

--- frontend/src/game/Piece/Pawn/Pawn.test.js
+++ frontend/src/game/Piece/Pawn/Pawn.test.js
@@ -19,8 +19,8 @@ const moveToEmptySquare = () => {
 
 test('pawn can move up 1 square', () => {
     moveToEmptySquare();
-    expect(whitePawn.canMove([4, 4], [3, 4])).toBe(true);
-    expect(blackPawn.canMove([5, 3], [6, 3])).toBe(true);
+    expect(whitePawn.canMove([3, 4], [4, 4])).toBe(true);
+    expect(blackPawn.canMove([6, 3], [5, 3])).toBe(true);
 });
 
 test('pawn can move up 2 square', () => {
@@ -29,38 +29,38 @@ test('pawn can move up 2 square', () => {
     expect(blackPawn.canMove([3, 4], [5, 4])).toBe(true);
 });
 
-// test('pawn can move to a square with an enemy piece diagonal', () => {
-//     boardState.getPiece.mockReturnValueOnce(blackPiece).mockReturnValue(whitePiece);
-//     expect(whitePawn.canMove([0, 0], [1, 1])).toBe(true);
-//     expect(blackPawn.canMove([5, 1], [4, 2])).toBe(true);
-// });
+test('pawn can move to a square with an enemy piece diagonal', () => {
+    boardState.getPiece.mockReturnValueOnce(blackPiece).mockReturnValue(whitePiece);
+    expect(whitePawn.canMove([0, 0], [1, 1])).toBe(true);
+    expect(blackPawn.canMove([5, 1], [4, 2])).toBe(true);
+});
 
-// test('Pawn cannot move to a square with a piece belonging to the same player', () => {
-//     boardState.getPiece.mockReturnValueOnce(whitePiece).mockReturnValue(blackPiece);
-//     expect(whitePawn.canMove([2, 1], [3, 1])).toBe(false);
-//     expect(blackPawn.canMove([4, 1], [3, 1])).toBe(false);
-// });
+test('Pawn cannot move to a square with a piece belonging to the same player', () => {
+    boardState.getPiece.mockReturnValueOnce(whitePiece).mockReturnValue(blackPiece);
+    expect(whitePawn.canMove([2, 1], [3, 1])).toBe(false);
+    expect(blackPawn.canMove([4, 1], [3, 1])).toBe(false);
+});
 
-// test('pawn cannot move horizontally', () => {
-//     moveToEmptySquare();
-//     expect(blackPawn.canMove([0, 0], [0, 2])).toBe(false);
-//     expect(whitePawn.canMove([0, 5], [0, 1])).toBe(false);
-// });
+test('pawn cannot move horizontally', () => {
+    moveToEmptySquare();
+    expect(blackPawn.canMove([0, 0], [0, 2])).toBe(false);
+    expect(whitePawn.canMove([0, 5], [0, 1])).toBe(false);
+});
 
-// test('Pawn cannot move to the same position', () => {
-//     moveToEmptySquare();
-//     expect(whitePawn.canMove([0, 0], [0, 0])).toBe(false);
-// });
+test('Pawn cannot move to the same position', () => {
+    moveToEmptySquare();
+    expect(whitePawn.canMove([0, 0], [0, 0])).toBe(false);
+});
 
-// test('Pawn cannot move backwards vertically', () => {
-//     moveToEmptySquare();
-//     expect(whitePawn.canMove([2, 1], [3, 1])).toBe(false);
-//     expect(blackPawn.canMove([4, 2], [5, 2])).toBe(false);
-// });
+test('Pawn cannot move backwards vertically', () => {
+    moveToEmptySquare();
+    expect(whitePawn.canMove([2, 1], [3, 1])).toBe(false);
+    expect(blackPawn.canMove([4, 2], [5, 2])).toBe(false);
+});
 
-// test('Pawn cannot move backwards diagonally', () => {
-//     moveToEmptySquare();
-//     expect(whitePawn.canMove([3, 1], [2, 0])).toBe(false);
-//     expect(blackPawn.canMove([4, 2], [5, 1])).toBe(false);
-// });
+test('Pawn cannot move backwards diagonally', () => {
+    moveToEmptySquare();
+    expect(whitePawn.canMove([3, 1], [2, 0])).toBe(false);
+    expect(blackPawn.canMove([4, 2], [5, 1])).toBe(false);
+});
 
