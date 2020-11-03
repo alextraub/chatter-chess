459dfb2611eb838d24b2955debb1e7f854fa2b78
Devon <trolle3.14@gmail.com>
2020-11-03 14:54:12 -0600

    Check tests working and passing
    remember, inCheck and inCheckMate return boolean answers to the question. ie Am I in check mate? false

--- frontend/src/game/Check/Check.test.js
+++ frontend/src/game/Check/Check.test.js
@@ -1,34 +1,62 @@
 import BoardState from '../BoardState'
-import Check from '.'
+import {inCheck, inCheckMate} from './'
 import King from '../Piece/King'
 import Rook from '../Piece/Rook'
-jest.mock('../../Game/BoardState')
+//jest.mock('../../Game/BoardState')
 
-beforeEach(() =>
-{
-    BoardState.mockClear();
-});
 
 const boardState = new BoardState();
-const check = new Check;
 const whiteKing = new King(boardState, 0);
 const blackKing = new King(boardState, 1);
 const whiteRook = new Rook(boardState, 0);
 const blackRook = new Rook(boardState, 1);
+function emptyBoard(boardstate)
+{
+    for(let r = 0; r < 8; ++r)
+    {
+        for(let c = 0; c < 8; ++c)
+        {
+            boardstate.board[r][c] = null;
+        }
+    }
+}
 
 test('it can tell when the King isnt in check', () =>
 {
+    emptyBoard(boardState);
     boardState.placePiece(blackKing, [7, 4]);
     boardState.placePiece(whiteKing, [0, 3]);
 
-    expect(check.inCheck([7, 4], blackKing.boardState, blackKing.player)).toBe(false);
-    expect(check.inCheck([0, 3], whiteKing.boardState, whiteKing.player)).toBe(false);
+    expect(inCheck([7, 4], blackKing.boardState, blackKing.player)).toBe(false);
+    expect(inCheck([0, 3], whiteKing.boardState, whiteKing.player)).toBe(false);
 })
 
 test('it can tell when the King is in check', () =>
 {
+    emptyBoard(boardState);
     boardState.placePiece(blackKing, [7, 4]);
+    boardState.placePiece(whiteRook, [0, 4]);
+
+    expect(inCheck([7, 4], blackKing.boardState, blackKing.player)).toBe(true);
+})
+
+test('it can tell when the king is not in check mate', () =>
+{
+    emptyBoard(boardState);
+    boardState.placePiece(whiteKing, [7, 4]);
     boardState.placePiece(blackRook, [0, 4]);
 
-    expect(Check.inCheck([7, 4], blackKing.boardState, blackKing.player)).toBe(false);
+    expect(inCheckMate([7, 4], whiteKing.boardState, whiteKing.player)).toBe(false);
+})
+
+test('it can tell when the king is in check mate', () =>
+{
+    emptyBoard(boardState);
+    boardState.placePiece(blackKing, [7, 4]);
+
+    boardState.placePiece(whiteRook, [0, 3]);
+    boardState.placePiece(whiteRook, [0, 4]);
+    boardState.placePiece(whiteRook, [0, 5]);
+
+    expect(inCheckMate([7, 4], blackKing.boardState, blackKing.player)).toBe(true);
 })
--- frontend/src/game/Check/index.js
+++ frontend/src/game/Check/index.js
@@ -8,9 +8,9 @@ import BoardState from '../BoardState'
         {
             for(let c=0; c<8; c++)                  //for every coleum
             {
-                if(boardstate.getPiece([r, c]).player != team)    //if the peice there is an enemy
+                if(search[r][c] != null && search[r][c].player != team)    //if the peice there is an enemy
                 {
-                    if (search[r][c].canMove([r, c], [Row, Col]))   //we check if it can move to our square
+                    if(search[r][c].canMove([r, c], [Row, Col]))   //we check if it can move to our square
                     {
                         return true;                //if it can we return true, we are in check
                     }
@@ -25,83 +25,83 @@ import BoardState from '../BoardState'
         
         if(Row-1 >= 0 && Col-1 >= 0)                //if the up left is on the board
         {
-            if(boardstate.getPiece([Row-1, Col-1]).player == team)//if the space doesn't have a allied piece
+            if(boardstate.getPiece([Row-1, Col-1]) == null || boardstate.getPiece([Row-1, Col-1]).player == team)//if the space doesn't have a allied piece
             {
-                if(!(this.inCheck([Row-1, Col-1], boardstate, team)))//if that position is not in check
+                if(!(inCheck([Row-1, Col-1], boardstate, team)))//if that position is not in check
                 {
-                    return true;                    //we can move to another space
+                    return false;                    //we can move to another space
                 }
             }
         }
         if(Row-1 >= 0)                              //if up is on the board
         {
-            if(boardstate.getPiece([Row-1, Col]).player == team)//if the space doesn't have a allied piece
+            if(boardstate.getPiece([Row-1, Col-1]) == null || boardstate.getPiece([Row-1, Col]).player == team)//if the space doesn't have a allied piece
             {
-                if(!(this.inCheck([Row-1, Col], boardstate, team)))//if that position is not in check
+                if(!(inCheck([Row-1, Col], boardstate, team)))//if that position is not in check
                 {
-                    return true;                    //we can move to another space
+                    return false;                    //we can move to another space
                 }
             }
         }
         if(Row-1 >= 0 && Col+1 < 8)                //if the up right is on the board
         {
-            if(boardstate.getPiece([Row-1, Col+1]).player == team)//if the space doesn't have a allied piece
+            if(boardstate.getPiece([Row-1, Col-1]) == null || boardstate.getPiece([Row-1, Col+1]).player == team)//if the space doesn't have a allied piece
             {
-                if(!(this.inCheck([Row-1, Col+1], boardstate, team)))//if that position is not in check
+                if(!(inCheck([Row-1, Col+1], boardstate, team)))//if that position is not in check
                 {
-                    return true;                    //we can move to another space
+                    return false;                    //we can move to another space
                 }
             }
         }
         if(Col+1 < 8)                              //if right is on the board
         {
-            if(boardstate.getPiece([Row, Col+1]).player == team)//if the space doesn't have a allied piece
+            if(boardstate.getPiece([Row-1, Col-1]) == null || boardstate.getPiece([Row, Col+1]).player == team)//if the space doesn't have a allied piece
             {
-                if(!(this.inCheck([Row, Col+1], boardstate, team)))//if that position is not in check
+                if(!(inCheck([Row, Col+1], boardstate, team)))//if that position is not in check
                 {
-                    return true;                    //we can move to another space
+                    return false;                    //we can move to another space
                 }
             }
         }
         if(Row+1 < 8 && Col+1 < 8)                //if down right is on the board
         {
-            if(boardstate.getPiece([Row+1, Col+1]).player == team)//if the space doesn't have a allied piece
+            if(boardstate.getPiece([Row-1, Col-1]) == null || boardstate.getPiece([Row+1, Col+1]).player == team)//if the space doesn't have a allied piece
             {
-                if(!(this.inCheck([Row+1, Col+1], boardstate, team)))//if that position is not in check
+                if(!(inCheck([Row+1, Col+1], boardstate, team)))//if that position is not in check
                 {
-                    return true;                    //we can move to another space
+                    return false;                    //we can move to another space
                 }
             }
         }
         if(Row+1 < 8)                              //if down is on the board
         {
-            if(boardstate.getPiece([Row+1, Col]).player == team)//if the space doesn't have a allied piece
+            if(boardstate.getPiece([Row-1, Col-1]) == null || boardstate.getPiece([Row+1, Col]).player == team)//if the space doesn't have a allied piece
             {
-                if(!(this.inCheck([Row+1, Col], boardstate, team)))//if that position is not in check
+                if(!(inCheck([Row+1, Col], boardstate, team)))//if that position is not in check
                 {
-                    return true;                    //we can move to another space
+                    return false;                    //we can move to another space
                 }
             }
         }
         if(Row+1 < 8 && Col-1 >= 0)                //if the down left is on the board
         {
-            if(boardstate.getPiece([Row+1, Col-1]).player == team)//if the space doesn't have a allied piece
+            if(boardstate.getPiece([Row-1, Col-1]) == null || boardstate.getPiece([Row+1, Col-1]).player == team)//if the space doesn't have a allied piece
             {
-                if(!(this.inCheck([Row+1, Col-1], boardstate, team)))//if that position is not in check
+                if(!(inCheck([Row+1, Col-1], boardstate, team)))//if that position is not in check
                 {
-                    return true;                    //we can move to another space
+                    return false;                    //we can move to another space
                 }
             }
         }
         if(Col-1 >= 0)                              //if left is on the board
         {
-            if(boardstate.getPiece([Row-1, Col-1]).player == team)//if the space doesn't have a allied piece
+            if(boardstate.getPiece([Row-1, Col-1]) == null || boardstate.getPiece([Row-1, Col-1]).player == team)//if the space doesn't have a allied piece
             {
-                if(!(this.inCheck([Row-1, Col-1], boardstate, team)))//if that position is not in check
+                if(!(inCheck([Row-1, Col-1], boardstate, team)))//if that position is not in check
                 {
-                    return true;                    //we can move to another space
+                    return false;                    //we can move to another space
                 }
             }
         }
-        return false; //we have nowhere to move to, thus we are in check
+        return true; //we have nowhere to move to, thus we are in check
     }
