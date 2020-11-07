8f815e1a380bb782912bf24eec0e9a554f662e1e
Alex Traub <abtraub@uwm.edu>
2020-11-07 12:33:10 -0600

    Wrote more GameContainer tests

--- frontend/src/components/CapturedPieces/index.js
+++ frontend/src/components/CapturedPieces/index.js
@@ -30,13 +30,13 @@ export default class CapturedPieces extends React.Component {
 
 		return (
 			<div className="captured-container" data-testid="capturedContainer">
-				<h2>Captured ({pieces.count})</h2><br/>
+				<h2 data-testid={`${black ? 'black' : 'white'}-captured-total`}>Captured ({pieces.count})</h2><br />
 				<ul>
 					{Object.entries(pieces.pieces)
 						.filter(entry => entry[1].length > 0)
 						.map(([type, pieceArray]) => (
 							<li data-testid="captured-graphic" key={`${type}s`} className="captured-graphic">
-								<ChessPiece type={type} black={black}/>
+								<ChessPiece type={type} black={black} />
 								<span className="captured-count">x{pieceArray.length}</span>
 							</li>
 						))}
--- frontend/src/components/GameContainer/GameContainer.test.js
+++ frontend/src/components/GameContainer/GameContainer.test.js
@@ -44,6 +44,15 @@ test('GameContainer is rendered', () => {
 	expect(screen.getAllByTestId('game-container')).toHaveLength(1);
 });
 
+const makeMove = moveString => {
+	fireEvent.change(screen.getByTestId('move'), {
+		target: {
+			value: moveString
+		}
+	});
+	fireEvent.click(screen.getByTestId('button'));
+}
+
 const renderCheckScenario1 = moveString => {
 	const bState = new BoardState();
 
@@ -68,12 +77,7 @@ const renderCheckScenario1 = moveString => {
 	changePosition([7,6], [0,3], bState);
 
 	render(<GameContainer turn={1} boardState={bState} />);
-	fireEvent.change(screen.getByTestId('move'), {
-		target: {
-			value: moveString
-		}
-	});
-	fireEvent.click(screen.getByTestId('button'));
+	makeMove(moveString);
 
 	return bState;
 }
@@ -94,4 +98,29 @@ test('Capturing a piece to get out of check does not cause an error message to b
 	const bState = renderCheckScenario1('a5 A6')
 	expect(screen.getByTestId('error')).toBeEmptyDOMElement();
 	expect(bState.getPiece([0,5]).type).toEqual('king');
+});
+
+test('Fool\'s mate', () => {
+	render(<GameContainer />);
+	makeMove('G6 F6');
+	makeMove('b5 d5');
+	makeMove('G7 e7');
+	makeMove('a4 E8');
+
+	expect(screen.getByTestId('move')).toBeDisabled();
+	expect(screen.getByTestId('button')).toBeDisabled();
+	expect(screen.getByTestId('winner')).toHaveTextContent('White wins');
+});
+
+test('Total captured count updates', () => {
+	render(<GameContainer />);
+
+	makeMove('G1 E1');
+	makeMove('B2 D2');
+
+	expect(screen.getByTestId('black-captured-total')).toHaveTextContent('Captured (0)');
+
+	makeMove('E1 D2');
+
+	expect(screen.getByTestId('black-captured-total')).toHaveTextContent('Captured (1)');
 });
