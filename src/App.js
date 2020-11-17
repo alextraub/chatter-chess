54527ffcb5e4b4ac59882480888184d4bc0a2310
Alex Traub <abtraub@uwm.edu>
2020-11-16 18:25:43 -0600

    Adding login button back

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import GameContainer from './components/GameContainer';
-// import UserHub from './components/UserHub';
+import UserHub from './components/UserHub';
 
 const App = () => (
 	<div>
-		{/* <UserHub /> */}
+		<UserHub />
 		<GameContainer />
 		<p>Credits</p>
 		Chess piece graphics by <a href="https://en.wikipedia.org/wiki/User:Cburnett">Colin M.L. Burnett</a>
