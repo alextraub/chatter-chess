b31cb554f18c7510c91737ef65389e8e4e2255fd
Alex Traub <abtraub@uwm.edu>
2020-11-23 14:38:51 -0600

    Config bugs

--- src/index.js
+++ src/index.js
@@ -5,10 +5,47 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Amplify from "aws-amplify";
-import awsConfig from "./awsConfig";
+import awsConfig from "./aws-exports";
 
 
-Amplify.configure(awsConfig);
+let isDevelopment = false;
+try {
+	isDevelopment = process.env.NODE_ENV === 'development';
+} catch (err) {
+	console.error('Autherization source is not allowed');
+}
+
+const isLocalhost = isDevelopment && (
+	window.location.hostname === "localhost" ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === "[::1]" ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+// Assuming you have two redirect URIs, and the first is for localhost and second is for production
+const [
+	localRedirectSignIn,
+	productionRedirectSignIn
+] = awsConfig.oauth.redirectSignIn.split(",");
+
+const [
+	localRedirectSignOut,
+	productionRedirectSignOut
+] = awsConfig.oauth.redirectSignOut.split(",");
+
+const modifiedConfig = {
+	...awsConfig,
+	oauth: {
+		...awsConfig.oauth,
+		redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
+		redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut
+	}
+}
+
+
+Amplify.configure(modifiedConfig);
 
 ReactDOM.render(
 	<App />,
