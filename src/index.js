7a2c1faeb7d82bd956eb18e5e243a4f706c9d690
Alex Traub <abtraub@uwm.edu>
2020-11-23 18:02:26 -0600

    Reworked production and development redirect uri config

--- src/index.js
+++ src/index.js
@@ -7,8 +7,32 @@ import reportWebVitals from './reportWebVitals';
 import Amplify from "aws-amplify";
 import awsConfig from './aws-exports';
 
-console.log(awsConfig);
 Amplify.configure(awsConfig);
+
+let isDevelopment = false;
+try {
+	isDevelopment = process.env.NODE_ENV === 'development';
+} catch (err) {
+	console.error('Autherization source is not allowed');
+}
+
+const isLocalhost = isDevelopment && (
+	window.location.hostname === "localhost" ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === "[::1]" ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+if(isLocalhost) {
+	Amplify.Auth._config.oauth = {
+		...Amplify.Auth._config.oauth,
+		redirectSignIn: 'http://localhost:3000/',
+		redirectSignOut: 'http://localhost:3000/'
+	}
+}
+
 
 ReactDOM.render(
 	<App />,
