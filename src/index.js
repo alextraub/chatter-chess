505a3c9eed145653e94b106f0c4dbfd47e4b4674
Alex Traub <abtraub@uwm.edu>
2020-11-23 18:13:38 -0600

    Bug fixes

--- src/index.js
+++ src/index.js
@@ -7,32 +7,37 @@ import reportWebVitals from './reportWebVitals';
 import Amplify from "aws-amplify";
 import awsConfig from './aws-exports';
 
-Amplify.configure(awsConfig);
-
-let isDevelopment = false;
-try {
-	isDevelopment = process.env.NODE_ENV === 'development';
-} catch (err) {
-	console.error('Autherization source is not allowed');
-}
-
-const isLocalhost = isDevelopment && (
+const isLocalhost = Boolean(
 	window.location.hostname === "localhost" ||
     // [::1] is the IPv6 localhost address.
     window.location.hostname === "[::1]" ||
     // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    window.location.hostname.match(
+    	/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
 );
 
-if(isLocalhost) {
-	Amplify.Auth._config.oauth = {
-		...Amplify.Auth._config.oauth,
-		redirectSignIn: 'http://localhost:3000/',
-		redirectSignOut: 'http://localhost:3000/'
+// Assuming you have two redirect URIs, and the first is for localhost and second is for production
+const [
+	productionRedirectSignIn,
+	localRedirectSignIn
+] = awsConfig.oauth.redirectSignIn.split(",");
+
+const [
+	productionRedirectSignOut,
+	localRedirectSignOut
+] = awsConfig.oauth.redirectSignOut.split(",");
+
+const updatedAwsConfig = {
+	...awsConfig,
+	oauth: {
+		...awsConfig.oauth,
+		redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
+		redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut
 	}
 }
 
+Amplify.configure(updatedAwsConfig);
 
 ReactDOM.render(
 	<App />,
