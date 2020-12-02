88916b6d0c24842056c62d24ebb98be232175373
Alex Traub <abtraub@uwm.edu>
2020-12-01 19:06:56 -0600

    Readded alexa skill lambda function

---
+++ amplify/backend/function/ccalexaskill/src/index.js
@@ -0,0 +1,40 @@
+/* Amplify Params - DO NOT EDIT
+	API_GAMES_GRAPHQLAPIENDPOINTOUTPUT
+	API_GAMES_GRAPHQLAPIIDOUTPUT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+const Alexa = require('ask-sdk-core');
+const LaunchRequestHandler = require('./handlers/launchRequestIntentHandler');
+const HelloWorldIntentHandler = require('./handlers/helloWorldIntentHandler');
+const HelpIntentHandler = require('./handlers/helpIntentHandler');
+const CancelAndStopIntentHandler = require('./handlers/cancelAndStopIntentHandler');
+const SessionEndedRequestHandler = require('./handlers/sessionEndedRequestHandler');
+const ErrorHandler = require('./handlers/errorHandler');
+
+let skill;
+
+exports.handler = async function (event, context) {
+	// TODO implement
+	console.log(`REQUEST++++${JSON.stringify(event)}`);
+
+	if (!skill) {
+		skill = Alexa.SkillBuilders.custom()
+			.addRequestHandlers(
+				LaunchRequestHandler,
+				HelloWorldIntentHandler,
+				HelpIntentHandler,
+				CancelAndStopIntentHandler,
+				SessionEndedRequestHandler
+			)
+			.addErrorHandlers(ErrorHandler)
+			.create();
+	}
+
+	const response = await skill.invoke(event, context);
+	console.log(`RESPONSE++++${JSON.stringify(response)}`);
+
+	return response;
+
+};
