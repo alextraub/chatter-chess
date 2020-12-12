35dba57f27880da79d05cccc06e8b53b5909baea
alecschley <aschley@uwm.edu>
2020-12-12 16:07:25 -0600

    worked on Game and Database

--- src/components/GameContainer/index.js
+++ src/components/GameContainer/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect, useContext, useCallback} from 'react';
 
 import MoveInput from '../MoveInput';
 import BoardComponent from '../BoardComponent';
@@ -8,61 +8,83 @@ import { Col, Row } from 'reactstrap';
 import PropTypes from 'prop-types';
 
 import GameState from '../../game/GameState';
+import {API} from "aws-amplify";
+import * as queries from "../../graphql/queries";
+import {GRAPHQL_AUTH_MODE} from "@aws-amplify/api";
+import {AuthContext} from "../auth/AuthProvider";
 
 /**
  * Container component for a single instance of a chess game. It mantains the top level state
  * as well as renders the top-level UI components for the game.
  */
-class GameContainer extends React.Component {
-	static propTypes = {
-		gameState: PropTypes.instanceOf(GameState), // Used for testing
-		location: PropTypes.object
-	};
-
-	constructor(props) {
-		super(props);
-
-		// Set the game state for this instance
-		this.gameState = this.props.gameState ?
-			this.props.gameState : new GameState(0, require('../../game/BoardState/boards/standardGame').default);
-
-		// Use the game state to initialize the React state
-		this.state = {
-			turn: this.gameState.turn,
-			capturedPieces: this.gameState.getCapturedPieces(),
-			check: this.gameState.check,
-			board: this.gameState.board,
-			swapping: false,
-			swapList: []
-		}
-
-		/* Bind methods to this */
-		this.syncGame = this.syncGame.bind(this);
-		this.performMove = this.performMove.bind(this);
-		this.performPromotion = this.performPromotion.bind(this);
-	}
+const GameContainer = (props) => {
+	const auth = useContext(AuthContext); // Access the globally authenticated user
+	const [state, setState] = useState();
+	const [gameState, setGameState] = useState(props.gameState);
 
 	/**
 	 * Updates the React state to reflect the instance's game state property
 	 */
-	async syncGame() {
-		await this.setState({
-			...this.state,
-			turn: this.gameState.turn,
-			capturedPieces: this.gameState.getCapturedPieces(),
-			check: this.gameState.check,
-			board: this.gameState.board,
-			swapping: this.gameState.swapping,
-			swapList: this.gameState.swapList
+	const syncGame = () => {
+		setState({
+			...state,
+			turn: gameState.turn,
+			capturedPieces: gameState.getCapturedPieces(),
+			check: gameState.check,
+			board: gameState.board,
+			swapping: gameState.swapping,
+			swapList: gameState.swapList
 		});
-	}
+	};
+
+	const fetchGame = useCallback(async () => {
+		const {id} = props.match.params;
+		try {
+			const gameData = await API.graphql({
+				query: queries.getGame,
+				variables: {
+					id
+				},
+				authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS
+			}); // Fetch the data from the API
+			setGameState(new GameState(gameData.turn, gameData.pieces, {
+				WHITE: {...gameData.checkStatusWhite},
+				BLACK: {...gameData.checkStatusBlack}
+			}));
+			syncGame();
+		} catch (error) {
+			console.log(error);
+		}
+
+	}, [syncGame, auth, props.match]);
+
+	useEffect(() => {
+		if (!gameState) {
+			fetchGame();
+		}
+	}, [gameState, fetchGame]);
+
+	// /**
+	//  * Updates the React state to reflect the instance's game state property
+	//  */
+	// const syncGame = () => {
+	// 	setState({
+	// 		...state,
+	// 		turn: gameState.turn,
+	// 		capturedPieces: gameState.getCapturedPieces(),
+	// 		check: gameState.check,
+	// 		board: gameState.board,
+	// 		swapping: gameState.swapping,
+	// 		swapList: gameState.swapList
+	// 	});
+	// };
 
 	/**
 	 * Get a numeric value to represent which player's turn it is
 	 * @returns {0|1} 0 for white, 1 for black
 	 */
-	get player() {
-		return this.state.turn % 2;
+	const currentPlayer = () => {
+		return state.turn % 2;
 	}
 
 	/**
@@ -72,9 +94,9 @@ class GameContainer extends React.Component {
 	 * @param {[number, number]} to position to move to
 	 * @returns {boolean}
 	 */
-	async performMove(from, to) {
-		const result = this.gameState.performMove(from, to);
-		await this.syncGame();
+	const performMove = async(from, to) => {
+		const result = gameState.performMove(from, to);
+		syncGame();
 		return result;
 	}
 
@@ -84,17 +106,17 @@ class GameContainer extends React.Component {
 	 *
 	 * @param {string} type the type of piece to promote the piece waiting to be promoted to.
 	 */
-	async performPromotion(type) {
-		this.gameState.performPromotion(type);
-		await this.syncGame();
+	const performPromotion = async(type) => {
+		gameState.performPromotion(type);
+		syncGame();
 	}
 
 	/**
 	 * Returns all the UI elements that never are hidden
 	 * @todo disable move input during a piece swap
 	 */
-	renderStandardUI() {
-		const { swapping, check } = this.state;
+	const renderStandardUI = () => {
+		const { swapping, check } = state;
 		const isSwapping = swapping !== false;
 		const gameOver = check.WHITE.mate || check.BLACK.mate;
 
@@ -103,35 +125,35 @@ class GameContainer extends React.Component {
 			<Row>
 				<Col md="12">
 					<MoveInput
-						inCheck={this.gameState.isInCheck}
-						currentPlayer={this.player}
-						getPiece={this.gameState.getPiece}
-						performMove={this.performMove}
+						inCheck={gameState.isInCheck}
+						currentPlayer={currentPlayer()}
+						getPiece={gameState.getPiece}
+						performMove={performMove}
 						disabled={isSwapping || gameOver}
 					/>
 					{gameOver ? <span data-testid="winner">{check.WHITE.mate ? 'Black' : 'White'} wins!</span> : ''}</Col>
 				<Col md="12">
 					<CapturedPieces
-						black={this.player !== 0}
-						pieces={this.player !== 1 ?
-							this.state.capturedPieces.WHITE :
-							this.state.capturedPieces.BLACK}
+						black={currentPlayer() !== 0}
+						pieces={currentPlayer() !== 1 ?
+							state.capturedPieces.WHITE :
+							state.capturedPieces.BLACK}
 						className="list-group-horizontal"
 					/>
 				</Col>
 				<Col md="12">
 					<BoardComponent
 						id="board"
-						player={this.player}
-						board={this.state.board}
+						player={currentPlayer()}
+						board={state.board}
 					/>
 				</Col>
 				<Col md="12">
 					<CapturedPieces
-						black={this.player === 0}
-						pieces={this.player === 1 ?
-							this.state.capturedPieces.WHITE :
-							this.state.capturedPieces.BLACK}
+						black={currentPlayer() === 0}
+						pieces={currentPlayer() === 1 ?
+							state.capturedPieces.WHITE :
+							state.capturedPieces.BLACK}
 						className="list-group-horizontal"
 					/>
 				</Col>
@@ -139,18 +161,22 @@ class GameContainer extends React.Component {
 		);
 	}
 
-	render() {
-		console.log(this.props.location);
-		return (
-			<div data-testid="game-container">
-				<SwapPieces
-					open={this.state.swapping !== false}
-					swapList={this.state.swapList}
-					performPromotion={type => {this.performPromotion(type)}} />
-				{this.renderStandardUI()}
-			</div>
-		);
-	}
+
+	return (
+		<div data-testid="game-container">
+			<SwapPieces
+				open={state.swapping !== false}
+				swapList={state.swapList}
+				performPromotion={type => {performPromotion(type)}} />
+			{renderStandardUI()}
+		</div>
+	);
+
+}
+
+GameContainer.propTypes = {
+	gameState: PropTypes.instanceOf(GameState), // Used for testing
+	match: PropTypes.object
 }
 
 
