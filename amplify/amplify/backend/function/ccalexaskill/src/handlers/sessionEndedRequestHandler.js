aaba1a5d2420e714150f2e6fb10176fdce489adf
Alex Traub <abtraub@uwm.edu>
2020-12-13 13:04:09 -0600

    More version fixes

--- amplify/amplify/backend/function/ccalexaskill/src/handlers/sessionEndedRequestHandler.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	canHandle(handlerInput) {
-		return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
-	},
-	handle(handlerInput) {
-		//any cleanup logic goes here
-		return handlerInput.responseBuilder.getResponse();
-	}
-};
